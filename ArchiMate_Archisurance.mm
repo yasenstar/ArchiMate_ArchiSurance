ebbb054754efb136d2db0b53a12b1641894fe3cf
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-02-15 23:44:55 +0800

    initialization and prepare for beginning

--- ArchiMate_Archisurance.mm
+++ ArchiMate_Archisurance.mm
@@ -1,7 +1,7 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="ArchiMate_Archisurance" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1734354826723"><hook NAME="MapStyle" background="#f5f5dcff">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" fit_to_viewport="false"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES"/>
     <tags category_separator="::"/>
 
 <map_styles>
@@ -9,7 +9,7 @@
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.6 pt;-9.9 pt;" ENDINCLINATION="81.6 pt;19.8 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.75 pt;-9.375 pt;" ENDINCLINATION="81.75 pt;19.875 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
@@ -82,10 +82,7 @@
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <font BOLD="true"/>
 <node TEXT="1. Introduction" FOLDED="true" POSITION="bottom_or_right" ID="ID_1562131704" CREATED="1734355105431" MODIFIED="1734355109587">
-<node TEXT="Lecture 01 Opening and Introduction" ID="ID_266376583" CREATED="1734356652401" MODIFIED="1734356675108">
-<icon BUILTIN="video"/>
-</node>
-<node TEXT="1.1 The ArchiMate and TOGAF Standards" ID="ID_132273787" CREATED="1734355226619" MODIFIED="1734355238467">
+<node TEXT="1.1 The ArchiMate and TOGAF Standards" FOLDED="true" ID="ID_132273787" CREATED="1734355226619" MODIFIED="1734355238467">
 <node TEXT="The OpenGroup" ID="ID_699094964" CREATED="1734747137674" MODIFIED="1734747147141" LINK="https://www.opengroup.org/"/>
 <node TEXT="TOGAF, version 10" ID="ID_1155697080" CREATED="1734747148142" MODIFIED="1734747194926" LINK="https://www.opengroup.org/togaf"/>
 <node TEXT="ArchiMate 3.2" ID="ID_222025945" CREATED="1734747209059" MODIFIED="1734747227170" LINK="https://publications.opengroup.org/c226?_gl=1*817203*_ga*MTEyNjcyODEzNi4xNzM0MzUyNzU2*_ga_LTTWTXF7XY*MTczNDc0NzEzNi4yLjEuMTczNDc0NzE2Ny4wLjAuMA..*_ga_R80QG90XKM*MTczNDc0NzEzOC4yLjEuMTczNDc0NzE3MC4wLjAuMA..*_ga_2C9VJRZZ38*MTczNDc0NzEzOS4yLjEuMTczNDc0NzE3MC4wLjAuMA..*_ga_KLT018BLSC*MTczNDc0NzEzOS4yLjEuMTczNDc0NzE3MC4wLjAuMA..*_ga_51ESKVT3R5*MTczNDc0NzEzOS4zLjEuMTczNDc0NzE3MC4wLjAuMA..*_ga_7KXKND8JE1*MTczNDc0NzEzOS4zLjEuMTczNDc0NzE3MC4wLjAuMA..&amp;_ga=2.195283756.1445296239.1734747140-1126728136.1734352756"/>
@@ -94,32 +91,86 @@
 <node TEXT="1.3 Rationalization" ID="ID_1189608158" CREATED="1734355242240" MODIFIED="1734355247576"/>
 <node TEXT="1.4 Digital Transformation" ID="ID_671390210" CREATED="1734355247726" MODIFIED="1734355253952"/>
 </node>
-<node TEXT="2. Phase A: Architecture Vision" FOLDED="true" POSITION="bottom_or_right" ID="ID_994557945" CREATED="1734355109748" MODIFIED="1734355116749">
-<node TEXT="2.1 Longer-Term Vision" ID="ID_1301363047" CREATED="1734355255203" MODIFIED="1734355261197"/>
-<node TEXT="2.2 Solution Concept" ID="ID_1305818959" CREATED="1734355261365" MODIFIED="1734355265775"/>
+<node TEXT="2. Phase A: Architecture Vision" POSITION="bottom_or_right" ID="ID_994557945" CREATED="1734355109748" MODIFIED="1734355116749">
+<node TEXT="2.0 Overview" ID="ID_524917807" CREATED="1739626225159" MODIFIED="1739626230708">
+<node TEXT="Figure 3: Fragment of a Stakeholder View" ID="ID_1255164184" CREATED="1739626230712" MODIFIED="1739626244479"/>
+<node TEXT="Figure 4: Business Goals, Assessments, and Expected Outcomes Associated with the Driver Profitability" ID="ID_345326392" CREATED="1739626249363" MODIFIED="1739626280405"/>
+<node TEXT="Figure 5: Principles View (Fragment)" ID="ID_892935377" CREATED="1739626317437" MODIFIED="1739626331730"/>
+<node TEXT="Figure 6: Goal Refinement View for Rationalization Strategy" ID="ID_1898609846" CREATED="1739626334448" MODIFIED="1739626349785"/>
+</node>
+<node TEXT="2.1 Longer-Term Vision" FOLDED="true" ID="ID_1301363047" CREATED="1734355255203" MODIFIED="1734355261197">
+<node TEXT="Figure 7: Strategy View for Digital Customer Intimacy Strategy" ID="ID_186266214" CREATED="1739626362813" MODIFIED="1739626376368"/>
+</node>
+<node TEXT="2.2 Solution Concept" FOLDED="true" ID="ID_1305818959" CREATED="1734355261365" MODIFIED="1734355265775">
+<node TEXT="Figure 8: Solution Concept View" ID="ID_326028715" CREATED="1739626383596" MODIFIED="1739626392866"/>
+</node>
 </node>
 <node TEXT="3. Phase B: Business Architecture" FOLDED="true" POSITION="bottom_or_right" ID="ID_1104497762" CREATED="1734355116925" MODIFIED="1734355125117">
-<node TEXT="3.1 Organization Structure" ID="ID_1329494650" CREATED="1734355266589" MODIFIED="1734355272863"/>
-<node TEXT="3.2 Value Stream" ID="ID_93733105" CREATED="1734355273040" MODIFIED="1734355276702"/>
-<node TEXT="3.3. Capabilities" ID="ID_1349954583" CREATED="1734355276851" MODIFIED="1734355281779"/>
-<node TEXT="3.4 Business Functions" ID="ID_547664862" CREATED="1734355282027" MODIFIED="1734355287189"/>
-<node TEXT="3.5 Capabilities versus Business Functions" ID="ID_1621203621" CREATED="1734355287356" MODIFIED="1734355295891"/>
-<node TEXT="3.6 Business Processes" ID="ID_952165043" CREATED="1734355296298" MODIFIED="1734355304503"/>
-<node TEXT="3.7 Requirements Realization" ID="ID_1872682994" CREATED="1734355305150" MODIFIED="1734355326671"/>
-<node TEXT="3.8 Gap Analysis" ID="ID_1676240971" CREATED="1734355326827" MODIFIED="1734355331387"/>
-<node TEXT="3.9 Capability Realization" ID="ID_593508486" CREATED="1734355331560" MODIFIED="1734355340281"/>
+<node TEXT="3.1 Organization Structure" ID="ID_1329494650" CREATED="1734355266589" MODIFIED="1734355272863">
+<node TEXT="Figure 9: Organization View" ID="ID_947690422" CREATED="1739626409331" MODIFIED="1739626414951"/>
+<node TEXT="Figure 10: Organization Decomposition (Nested)" ID="ID_590009285" CREATED="1739626415109" MODIFIED="1739626427540"/>
 </node>
-<node TEXT="4. Phase C: Information Systems Architectures (Application)" POSITION="bottom_or_right" ID="ID_1238677738" CREATED="1734355125260" MODIFIED="1734355137937">
-<node TEXT="4.1 Application Coorperation" ID="ID_896635541" CREATED="1734355341339" MODIFIED="1734355351059"/>
-<node TEXT="4.2 Business-Application Alignment" ID="ID_780972834" CREATED="1734355351219" MODIFIED="1734355357205"/>
-<node TEXT="4.3 Application Behavior" ID="ID_452134743" CREATED="1734355357375" MODIFIED="1734355362323"/>
-<node TEXT="4.4 Application Gap Analysis" ID="ID_415250963" CREATED="1734355362493" MODIFIED="1734355368321"/>
+<node TEXT="3.2 Value Stream" ID="ID_93733105" CREATED="1734355273040" MODIFIED="1734355276702">
+<node TEXT="Figure 11: Value Stream" ID="ID_1677098564" CREATED="1739626431573" MODIFIED="1739626443993"/>
 </node>
-<node TEXT="5. Phase C: Information Systems Architectures (Data)" POSITION="bottom_or_right" ID="ID_1087622473" CREATED="1734355138095" MODIFIED="1734355150354"/>
-<node TEXT="6. Phase D: Technology Architecture" POSITION="bottom_or_right" ID="ID_940154546" CREATED="1734355150548" MODIFIED="1734355159389">
-<node TEXT="6.1 Infrastructure" ID="ID_1998277177" CREATED="1734355375094" MODIFIED="1734355384044"/>
-<node TEXT="6.2 Technology Gap Analysis" ID="ID_941018610" CREATED="1734355384418" MODIFIED="1734355403565"/>
+<node TEXT="3.3. Capabilities" ID="ID_1349954583" CREATED="1734355276851" MODIFIED="1734355281779">
+<node TEXT="Figure 12: Capability Map View (Baseline)" ID="ID_1364429720" CREATED="1739626463372" MODIFIED="1739626475453"/>
+<node TEXT="Figure 13: Value Stream - Capability Cross-Mapping" ID="ID_1570702721" CREATED="1739626483037" MODIFIED="1739626566199"/>
 </node>
-<node TEXT="7. Phase E &amp; F: Opportunities &amp; Solutions and Migration Planning" POSITION="bottom_or_right" ID="ID_1444568500" CREATED="1734355165418" MODIFIED="1734355183097"/>
+<node TEXT="3.4 Business Functions" ID="ID_547664862" CREATED="1734355282027" MODIFIED="1734355287189">
+<node TEXT="Figure 14: Business Function View (Baseline)" ID="ID_1855855022" CREATED="1739626573090" MODIFIED="1739626583342"/>
+</node>
+<node TEXT="3.5 Capabilities versus Business Functions" ID="ID_1621203621" CREATED="1734355287356" MODIFIED="1734355295891">
+<node TEXT="Figure 15: Capability Realization (Baseline, Partial)" ID="ID_584047103" CREATED="1739626591482" MODIFIED="1739626606708"/>
+</node>
+<node TEXT="3.6 Business Processes" ID="ID_952165043" CREATED="1734355296298" MODIFIED="1734355304503">
+<node TEXT="Figure 16: Business Process View (Baseline)" ID="ID_1381842950" CREATED="1739626610122" MODIFIED="1739626620462"/>
+<node TEXT="Figure 17: Value Streams are Realized by Business Processes" ID="ID_1249928929" CREATED="1739626623379" MODIFIED="1739626642463"/>
+</node>
+<node TEXT="3.7 Requirements Realization" ID="ID_1872682994" CREATED="1734355305150" MODIFIED="1734355326671">
+<node TEXT="Figure 18: (Partial) Requirements Realization View" ID="ID_439311373" CREATED="1739626650338" MODIFIED="1739626662111"/>
+</node>
+<node TEXT="3.8 Gap Analysis" ID="ID_1676240971" CREATED="1734355326827" MODIFIED="1734355331387">
+<node TEXT="Figure 19: Capability Gap Analysis" ID="ID_1276544628" CREATED="1739626667267" MODIFIED="1739626676656"/>
+</node>
+<node TEXT="3.9 Capability Realization" ID="ID_593508486" CREATED="1734355331560" MODIFIED="1734355340281">
+<node TEXT="Figure 20: Resource Map View (Target)" ID="ID_1719954894" CREATED="1739626682922" MODIFIED="1739626690590"/>
+<node TEXT="Figure 21: Resource Realization Vew (Target)" ID="ID_1637083893" CREATED="1739626693843" MODIFIED="1739626703326"/>
+</node>
+</node>
+<node TEXT="4. Phase C: Information Systems Architectures (Application)" FOLDED="true" POSITION="bottom_or_right" ID="ID_1238677738" CREATED="1734355125260" MODIFIED="1734355137937">
+<node TEXT="4.1 Application Coorperation" ID="ID_896635541" CREATED="1734355341339" MODIFIED="1734355351059">
+<node TEXT="Figure 22: Application Cooperation View (Baseline)" ID="ID_1837836553" CREATED="1739626721443" MODIFIED="1739626738174"/>
+<node TEXT="Figure 23: Application Cooperation View (Target)" ID="ID_1869535047" CREATED="1739626750234" MODIFIED="1739626762956"/>
+</node>
+<node TEXT="4.2 Business-Application Alignment" ID="ID_780972834" CREATED="1734355351219" MODIFIED="1734355357205">
+<node TEXT="Figure 24: Application Usage View (Baseline)" ID="ID_187025119" CREATED="1739626767337" MODIFIED="1739626779933"/>
+</node>
+<node TEXT="4.3 Application Behavior" ID="ID_452134743" CREATED="1734355357375" MODIFIED="1734355362323">
+<node TEXT="Figure 25: Application Behavior View (Target)" ID="ID_1872970224" CREATED="1739626784474" MODIFIED="1739626795388"/>
+</node>
+<node TEXT="4.4 Application Gap Analysis" ID="ID_415250963" CREATED="1734355362493" MODIFIED="1734355368321">
+<node TEXT="Figure 26: Application Architecture: Gap Analysis" ID="ID_138584412" CREATED="1739626800514" MODIFIED="1739626812110"/>
+</node>
+</node>
+<node TEXT="5. Phase C: Information Systems Architectures (Data)" FOLDED="true" POSITION="bottom_or_right" ID="ID_1087622473" CREATED="1734355138095" MODIFIED="1734355150354">
+<node TEXT="Figure 27: Information Structure View Showing Main Business Objects" ID="ID_1526584076" CREATED="1739626832194" MODIFIED="1739626851965"/>
+<node TEXT="Figure 28: Data Dissemination Diagram" ID="ID_825603183" CREATED="1739626853466" MODIFIED="1739626867436"/>
+</node>
+<node TEXT="6. Phase D: Technology Architecture" FOLDED="true" POSITION="bottom_or_right" ID="ID_940154546" CREATED="1734355150548" MODIFIED="1734355159389">
+<node TEXT="6.1 Infrastructure" ID="ID_1998277177" CREATED="1734355375094" MODIFIED="1734355384044">
+<node TEXT="Figure 29: Technology View (Baseline)" ID="ID_740271523" CREATED="1739626896345" MODIFIED="1739626905206"/>
+<node TEXT="Figure 30: Technology Architecture: Technology View (Target)" ID="ID_1763036866" CREATED="1739626908482" MODIFIED="1739626920932"/>
+<node TEXT="Figure 31: Data Acquisition from IoT Services (Target)" ID="ID_769394027" CREATED="1739626924858" MODIFIED="1739626939677"/>
+<node TEXT="Figure 32: IoT Device Services (Target)" ID="ID_1017925951" CREATED="1739626945497" MODIFIED="1739626955549"/>
+</node>
+<node TEXT="6.2 Technology Gap Analysis" ID="ID_941018610" CREATED="1734355384418" MODIFIED="1734355403565">
+<node TEXT="Figure 33: Technology Architecture: Gap Analysis" ID="ID_1842000785" CREATED="1739626961153" MODIFIED="1739626971078"/>
+</node>
+</node>
+<node TEXT="7. Phase E &amp; F: Opportunities &amp; Solutions and Migration Planning" FOLDED="true" POSITION="bottom_or_right" ID="ID_1444568500" CREATED="1734355165418" MODIFIED="1734355183097">
+<node TEXT="Figure 34; Migration View" ID="ID_700701965" CREATED="1739626978098" MODIFIED="1739626984238"/>
+<node TEXT="Figure 35: TOGAF Project Context Diagram, expressed in the ArchiMate Language" ID="ID_463206348" CREATED="1739626987180" MODIFIED="1739627009349"/>
+</node>
 </node>
 </map>
